ff9884c8476648ae8850322a593b8213fd9c8645
catmullah <mehd0114@hotmail.com>
2026-07-22 08:54:05 +0200

    now it makes squares, but off center

--- thePygame.py
+++ thePygame.py
@@ -3,21 +3,50 @@ import pygame as pg
 pg.init() # initialize the game
 
 # -- WINDOW SETTINGS -- #
-window = pg.display.set_mode((1440, 1080)) # make the window
+
+window_WIDTH = 600
+window_LENGTH = 600
+
+window = pg.display.set_mode((window_WIDTH, window_LENGTH)) # make the window
 pg.display.set_caption("WEEWOOWEEWOO") # set the window title
 
-window.fill((95, 128, 132))
-
 # -- PRE-PROCESSING -- #
+
+class Square():
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+    self.pressed = False
+
+  def make(self):
+    self.create = pg.draw.rect(
+      window, 
+      (0, 0, 255),
+      (window_WIDTH/2 + 80*self.x, window_LENGTH/2 + 80*self.y, 25, 25)
+    )
+    mousePos = pg.mouse.get_pos()
+    if self.create.collidepoint(mousePos):
+      pg.mouse.set_cursor(pg.cursors.tri_left)
+      if pg.mouse.get_pressed()[0] == 1 and self.pressed == False:
+        pg.mouse.set_cursor(pg.cursors.broken_x)
+        self.pressed = True
+        print("GET PRESSED!")
+    if pg.mouse.get_pressed()[0] == 0:
+      self.pressed = False
+
+
 stage = 1
 level = 1
 
-#grid = [
-#  pg.rec
-#]
+grid = []
+size = 7
 
-#for i in grid:
-  
+for i in range(size):
+  row = []
+  for j in range(size):
+    square = Square(j, i)
+    row.append(square)
+  grid.append(row)
 
 # -- KEEP THE GAME RUNNING + SETTINGS -- #
 run = True
@@ -25,7 +54,10 @@ while run:
   for event in pg.event.get():
     if event.type == pg.QUIT:
       run = False
-  pg.draw.rect(window, (0, 0, 255), (100, 100, 50, 50))
+  window.fill((95, 128, 132))
+  for row in grid:
+    for square in row:
+      square.make()
   pg.display.flip()
 
 # -- FOOTER -- #
